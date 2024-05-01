6262e4d23a8c7cb66977c8bc510b0d4304e371c8
Mouadamtti <158284959+Mouadamtti@users.noreply.github.com>
2024-05-01 20:32:02 +0100

    Update dl-apk.js

--- plugins/dl-apk.js
+++ plugins/dl-apk.js
@@ -1,61 +1,91 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-    if (!text) throw 'Ex: ' + usedPrefix + command + ' minecraft';
-    try {
-    await m.reply('*LOADING…*');
+  if (!args[0]) throw `*تحميل التطبيقات مع الملف الإضافي obb* \n\n مثـال :\n  ${usedPrefix}${command} firee fire`;
+  let info = await apkinfo(text);
+  let res = await apk(text);
 
-    let res = await apk(text);
-    
-    await conn.sendMessage(m.chat, {
-    image: { url: res.icon },
-    caption: `*Name:* ${res.name}\n*Downloads:* ${res.dc}\n*Package:* ${res.path}\n*File Size:* ${res.size}`,
+  if (res.size > 2000000000) {
+    throw '*ملف APK كبير جدًا. الحد الأقصى لحجم التنزيل هو 2GB ميجابايت*.';
+  }
+
+  let message = await conn.sendMessage(m.chat, {
+    image: { url: info.icon },
+    caption: `*App Name:* \n${info.name}\n*Package Name:* \n${info.packageN} \n\n> *ᴊɪᴛᴏssᴀ ʙᴇᴛᴀ 1.1.0ᴠ*`,
     footer: '_Apk files..._',
   });
-    
-    await m.reply(`UPLOADING : *${res.name}*`);
-    
-    const fileName = `${res.path}.${res.format}`;
-    await conn.sendMessage(
+
+ 
+  await conn.sendMessage(
     m.chat,
-    { document: { url: res.dl }, mimetype: res.mimetype, fileName: fileName },
+    { document: { url: res.download }, mimetype: res.mimetype, fileName: res.fileName },
     { quoted: m }
   );
-    } catch (error) {
-    await m.reply(`هناك ضغط على الموقع يرجى اعادة المحاولة لاحقا`);
-    }
-}
+
+  if (info.obb) {
+    await conn.sendMessage(m.chat, {
+      text: `جاري تحميل ملف OBB لـ ${info.name}...`,
+    });
+
+    let obbRes = await fetch(info.obb_link);
+    let obbMimetype = obbRes.headers.get('content-type');
+
+    await conn.sendMessage(
+      m.chat,
+      { document: { url: info.obb_link }, mimetype: obbMimetype, fileName: `${info.packageN}.obb` },
+      { quoted: m }
+    );
+  }
+
+  
+  //await conn.deleteMessage(m.chat, message.key.id)
+};
 
 handler.command = /^(apk)$/i;
 handler.help = ['apk'];
-handler.tags = ['downloader'];
-handler.premium = true;
+handler.tags = ['applications'];
+handler.premium = false;
+
 export default handler;
 
-async function apk(text) {
-  let response = await fetch(`https://energetic-charm-mastodon.glitch.me/search?q=${text}`);
-  let $ = await response.json();
-  let name = $.appName;
-  let icon = $.image;
-  let dl = $.Downloadlink;
-  let format = $.appFormat;
-  if(!dl) throw 'Can\'t download the apk!';
-  let dc = $.downloadCount;
-  let path = $.packageName;
-  let mimetype = (await fetch(dl, { method: 'head' })).headers.get('content-type');
-  const getsize = (await fetch(dl, { method: 'head' })).headers.get('Content-Length');
-  if (getsize > 500000000) {
-  throw 'حجم ملف apk كبير جدًا. الحد الأقصى لحجم التنزيل هو 500 ميغابايت.';
+async function apkinfo(url) {
+  let res = await fetch('http://ws75.aptoide.com/api/7/apps/search?query=' + url + '&limit=1');
+  let $ = await res.json();
+
+  try {
+    let icon = $.datalist.list[0].icon;
+  } catch {
+    throw 'تعذر تحميل التطبيق انا اسفة';
   }
-  let size = formatBytes(parseInt(getsize));
-  return { name, icon, dl, dc, path, format, size, mimetype}
+
+  let icon = $.datalist.list[0].icon;
+  let name = $.datalist.list[0].name;
+  let packageN = $.datalist.list[0].package;
+  let download = $.datalist.list[0].file.path;
+  let obb_link;
+  let obb;
+
+  try {
+    obb_link = await $.datalist.list[0].obb.main.path;
+    obb = true;
+  } catch {
+    obb_link = '_غير موجود_';
+    obb = false;
+  }
+
+  if (!download) throw 'تعذر تحميل التطبيق انا اسفة';
+  return { obb, obb_link, name, icon, packageN };
 }
 
-function formatBytes(bytes, decimals = 2) {
-    if (bytes === 0) return '0 Bytes';
-    const k = 1024;
-    const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+async function apk(url) {
+  let res = await fetch('http://ws75.aptoide.com/api/7/apps/search?query=' + encodeURIComponent(url) + '&limit=1');
+  let $ = await res.json();
+  let fileName = $.datalist.list[0].package + '.apk';
+  let download = $.datalist.list[0].file.path;
+  let size = (await fetch(download, { method: 'head' })).headers.get('Content-Length');
+  if (!download) throw 'Can\'t download the apk!';
+  let icon = $.datalist.list[0].icon;
+  let mimetype = (await fetch(download, { method: 'head' })).headers.get('content-type');
+
+  return { fileName, mimetype, download, size };
 }
